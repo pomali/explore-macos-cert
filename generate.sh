dbfa435283fe81a134c4fb3c7222a9b11d51ec14
pom <stanislavparnicky@gmail.com>
2021-11-10 16:23:10 +0100

    Working version

--- generate.sh
+++ generate.sh
@@ -1,2 +1,5 @@
 openssl req -nodes -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 \
-  -subj "/C=SK/ST=Test/L=Test/O=Test/CN=localhost.arcicode.com/"
+  -subj "/C=SK/ST=Test/L=Test/O=Test/CN=localhost.arcicode.com" \
+  -addext "subjectAltName = DNS:localhost.arcicode.com" \
+  -addext "certificatePolicies = 1.2.3.4"
+
--- install-cert.sh
+++ install-cert.sh
@@ -1,3 +1,9 @@
 
 #sudo security -v add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain cert.pem
-sudo security -v add-trusted-cert -d -e hostnameMismatch -r trustRoot -p basic -p ssl -k /Library/Keychains/System.keychain cert.pem
+security -v add-trusted-cert -d -r trustRoot -p basic -p ssl -k ~/Library/Keychains/login.keychain-db cert.pem
+
+# hostname mismatch
+# security -v add-trusted-cert -d -e hostnameMismatch -r trustRoot -p basic -p ssl -k ~/Library/Keychains/login.keychain-db cert.pem
+
+# global
+#sudo security -v add-trusted-cert -d -e hostnameMismatch -r trustRoot -p basic -p ssl -k /Library/Keychains/System.keychain cert.pem
