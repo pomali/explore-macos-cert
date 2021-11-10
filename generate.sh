05de0746c567fae07551f1c38e1d8597b71d7356
pom <>
2021-11-10 13:19:42 +0100

    WIP

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
-
+openssl req -nodes -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 \
+  -subj "/C=SK/ST=Test/L=Test/O=Test/CN=localhost/"
