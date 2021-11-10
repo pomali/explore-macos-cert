356df2cf4623b0c6386afa01605813c349d76cb2
pom <>
2021-11-10 14:57:47 +0100

    WIP

--- install-cert.sh
+++ install-cert.sh
@@ -1,1 +1,3 @@
-sudo security add-trusted-cert -d -r trustRoot -p basic -p ssl -k /Library/Keychains/System.keychain cert.pem
+
+#sudo security -v add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain cert.pem
+sudo security -v add-trusted-cert -d -e hostnameMismatch -r trustRoot -p basic -p ssl -k /Library/Keychains/System.keychain cert.pem
