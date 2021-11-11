1e63a6cb74df6071e7b9aeffcee03ab1fea25fac
pom <stanislavparnicky@gmail.com>
2021-11-11 09:48:33 +0100

    Add uninstall, remove admin flag

--- install-cert.sh
+++ install-cert.sh
@@ -1,7 +1,10 @@
+#!/bin/sh
+security -v add-trusted-cert -r trustRoot -p basic -p ssl -k ~/Library/Keychains/login.keychain-db cert.pem
+
+#security -v add-trusted-cert -d -r trustRoot -p basic -p ssl -k ~/Library/Keychains/login.keychain-db cert.pem
+
 
 #sudo security -v add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain cert.pem
-security -v add-trusted-cert -d -r trustRoot -p basic -p ssl -k ~/Library/Keychains/login.keychain-db cert.pem
-
 # hostname mismatch
 # security -v add-trusted-cert -d -e hostnameMismatch -r trustRoot -p basic -p ssl -k ~/Library/Keychains/login.keychain-db cert.pem
 
---
+++ uninstall-cert.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+security remove-trusted-cert ./cert.pem 
+#security remove-trusted-cert -d ./cert.pem 
